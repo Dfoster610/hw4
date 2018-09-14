dd622ac39b426301dfa284376113eaa9d64efe39
Dfoster610 <42472356+Dfoster610@users.noreply.github.com>
2018-09-14 12:26:27 -0400

    Add files via upload

---
+++ js/calcVolume.js
@@ -0,0 +1,33 @@
+//startProcess
+
+function startProcess() {
+    for (i = 0; i < 10; i++) {
+        Length = i+2;
+        Width = i+3;
+        Height = i+4;        
+        var volume = getVolume(Length, Width, Height);
+ 
+        if (volume <= 300) {
+            document.write('Small Volume ' + " " + volume + '<br>');
+        }
+
+        else if ((volume >= 301) && (volume < 601)) {
+            document.write('Medium Volume ' + volume + '<br>');
+        }
+        else if ((volume >= 601) && (volume < 1001)) {
+            document.write('Large Volume ' + volume + '<br>');
+        }
+        else if (volume >= 1001) {
+            document.write('Extra Large ' + 'Volume ' + volume + '<br>');
+        }  
+    }
+}
+startProcess();
+
+//getVolume
+    function getVolume(Length, Width, Height) {
+        var result = Length * Width * Height;
+        return result;
+    }
+   
+
